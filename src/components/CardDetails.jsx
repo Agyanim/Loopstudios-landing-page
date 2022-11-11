9dbb7b01111f5598ee54cdc4474507eee85e7a81
Agyanim <71318355+Agyanim@users.noreply.github.com>
2022-11-11 17:09:41 +0000

    mobile view background images added

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -6,6 +6,14 @@
  import Pocket from "../assets/images/desktop/image-pocket-borealis.jpg"
  import Curiosity from "../assets/images/desktop/image-curiosity.jpg"
  import Fisheye from "../assets/images/desktop/image-fisheye.jpg"
+ import FisheyeMobile from "../assets/images/mobile/image-fisheye.jpg"
+ import DeepEarthMobile from "../assets/images/mobile/image-deep-earth.jpg"
+ import NightArcadeMobile from "../assets/images/mobile/image-night-arcade.jpg"
+ import SoccerTeamMobile from "../assets/images/mobile/image-soccer-team.jpg"
+ import GridMobile from "../assets/images/mobile/image-grid.jpg"
+ import AboveMobile from "../assets/images/mobile/image-from-above.jpg"
+ import PocketMobile from "../assets/images/mobile/image-pocket-borealis.jpg"
+ import CuriosityMobile from "../assets/images/mobile/image-curiosity.jpg"
  
  const cardDetails = [
   {
@@ -58,3 +66,54 @@
   },
 ];
 export default cardDetails;
+
+export const cardDetailsMobile = [
+  {
+    image: 1,
+    imageName: {DeepEarthMobile},
+    prefix: "deep",
+    title: "Earth",
+  },
+  {
+    image: 2,
+    imageName:{NightArcadeMobile},
+    prefix: "Night",
+    title: "Arcade",
+  },
+  {
+    image: 3,
+    imageName: {SoccerTeamMobile},
+    prefix: "Soccer",
+    title: "Team vr",
+  },
+  {
+    image: 4,
+    imageName: {GridMobile},
+    prefix: "The",
+    title: "Grid",
+  },
+  {
+    image:5,
+    imageName:{AboveMobile},
+    prefix: "From up",
+    title: "Above-vr",
+  },
+  {
+    image: 6,
+  imageName: {PocketMobile},
+    prefix: "pocket",
+    title: "Borealis",
+  },
+  {
+    image: 7,
+    imageName: {CuriosityMobile},
+    prefix: "the",
+    title: "curiosity",
+  },
+  {
+    image: 8,
+    imageName:{FisheyeMobile},
+    prefix: "make it",
+    title: "fisheye",
+  },
+];
--- src/components/ImageCards.jsx
+++ src/components/ImageCards.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { ImageCard } from "./Cards";
 import cardDetails from "./CardDetails";
+import {cardDetailsMobile} from "./CardDetails";
+
 import "../component.css";
 
 
 const ImageCardsDesktop = () => {
   const myCard = cardDetails;
-  const imagePath = "/src/assets/images/desktop/";
-  // console.log(myCard[1].imageName.DeepEarth)
-
   const cards = myCard.map((value, index) => {
     const variable=myCard[index].imageName
-    const me= Object.values(variable)
-    console.log(me.at(0))
+    const image= Object.values(variable)
+    console.log(image.at(0))
     return (
       <ImageCard
         key={index}
-        imageName={me}
+        imageName={image}
         prefix={myCard[index].prefix}
         title={myCard[index].title}
       />
@@ -38,17 +37,21 @@ export default ImageCardsDesktop;
 
 export const ImageCardsMobile = () => {
   const myCard = cardDetails;
-  const imagePath = "/src/assets/images/mobile/";
   const cards = myCard.map((value, index) => {
+    const variable=myCard[index].imageName
+    const image= Object.values(variable)
+    console.log(image.at(0))
     return (
       <ImageCard
         key={index}
-        imageName={imagePath + myCard[index].imageName}
+        imageName={image}
         prefix={myCard[index].prefix}
         title={myCard[index].title}
       />
+      
     );
   });
+
 
   return (
 
